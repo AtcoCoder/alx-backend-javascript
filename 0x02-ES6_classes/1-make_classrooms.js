9f5ee8590511c725bd74f28e990b05f866e7ebd4
Omar Jammeh <orms45@gmail.com>
2024-04-11 20:46:19 +0000

    Task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from './0-classroom.js';
+
+export default function initiializeRoooms() {
+  const firstClass = new ClassRoom(19);
+  const secondClass = new ClassRoom(20);
+  const thirdClass = new ClassRoom(34);
+  return [firstClass, secondClass, thirdClass];
+}
