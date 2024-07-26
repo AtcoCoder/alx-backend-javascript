ef2064a03a508179bf35f47d9d5fa1aad2e54767
Omar Jammeh <orms45@gmail.com>
2024-07-26 02:09:54 +0000

    Add 5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (!(this.constructor === Building)) {
+      this.evacuationWarningMessage();
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
